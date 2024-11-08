89a90ebf0c42ec0b0c2e079a4227e231e8b267f1
paraigor <103633539+paraigor@users.noreply.github.com>
2024-11-08 19:59:10 +0300

    Create places app

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'where_to_go_frontend',
+    'where_to_go_frontend.apps.WhereToGoFrontendConfig',
+    'places.apps.PlacesConfig',
 ]
 
 MIDDLEWARE = [
