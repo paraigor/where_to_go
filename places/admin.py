f478f4128d71b203b328981f048ad9264ccfe8c7
paraigor <103633539+paraigor@users.noreply.github.com>
2024-11-09 22:12:59 +0300

    Add inline images to TourDetailes model

--- places/admin.py
+++ places/admin.py
@@ -3,12 +3,20 @@ from django.contrib import admin
 from .models import Tour, TourDetailes, TourImage
 
 
+class TourImageInline(admin.TabularInline):
+    model = TourImage
+    fields = ("image", "ordinal_number")
+    extra = 0
+
+
 @admin.register(TourDetailes)
 class TourDetailesAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {"fields": ("title", "description_short", "description_long")}),
         ("Координаты", {"fields": ("longitude", "latitude")}),
     )
+    inlines = [TourImageInline]
+
 
 @admin.register(Tour)
 class TourAdmin(admin.ModelAdmin):
