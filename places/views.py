5c4687d65b71d65b41d1891c75dec45b18bdbdf7
paraigor <103633539+paraigor@users.noreply.github.com>
2024-11-09 20:43:27 +0300

    Make JSON API return place detailes

--- places/views.py
+++ places/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Tour
+
+
+def place(request, place_id):
+    tour = get_object_or_404(
+        Tour.objects.prefetch_related("detailes").prefetch_related(
+            "detailes__images"
+        ),
+        pk=place_id,
+    )
+    tour_detailes = tour.detailes
+
+    response = {
+        "title": tour_detailes.title,
+        "imgs": [img.image.url for img in tour_detailes.images.all()],
+        "description_short": tour_detailes.description_short,
+        "description_long": tour_detailes.description_long,
+        "coordinates": {
+            "lng": tour_detailes.longitude,
+            "lat": tour_detailes.latitude,
+        },
+    }
+
+    return JsonResponse(
+        response,
+        safe=False,
+        json_dumps_params={"ensure_ascii": False, "indent": 4},
+    )
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -21,8 +21,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from where_to_go_frontend import views as index_views
+
 urlpatterns = [
-    path("", include("where_to_go_frontend.urls")),
+    path("", index_views.index),
+    path("places/", include("places.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
