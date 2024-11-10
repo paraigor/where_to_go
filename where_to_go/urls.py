a1f96494e745009cbc6e29caef3c58c4605e62f8
paraigor <103633539+paraigor@users.noreply.github.com>
2024-11-10 16:32:33 +0300

    Fix main urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
